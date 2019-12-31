8ebeb0861aa2cbfc387f11b053581b45c46019a1
Jan Wijffels <jwijffels@bnosac.be>
2019-12-31 11:02:03 +0100

    add type option in wordpiece_encode

--- R/wordpiece.R
+++ R/wordpiece.R
@@ -7,6 +7,8 @@
 #' @description Wordpiece encoding, usefull for BERT-style tokenisation. Experimental version mimicing class WordpieceTokenizer from \url{https://github.com/huggingface/transformers/blob/master/src/transformers/tokenization_bert.py}
 #' @param x a character vector with text which can be splitted based on white space to obtain words
 #' @param vocabulary a character vector of the vocabulary
+#' @param type a character string, either 'subwords' or 'ids' to get the subwords or the corresponding ids of these subwords as defined in the vocabulary of the model. 
+#' Defaults to 'subwords'.
 #' @param unk_token character string with a value for a token which is not part of the vocabulary. Defaults to '[UNK]'
 #' @param max_input_chars_per_word integer. A word which is longer than this specified number of characters will be set to the unknown token.
 #' @return a list of subword tokens
@@ -17,7 +19,11 @@
 #'                  vocabulary = c("un", "##aff", "##able"))
 #' wordpiece_encode(x = c("unaffable", "unaffableun", "unknown territory"), 
 #'                  vocabulary = c("un", "##aff", "##able", "##un")) 
-wordpiece_encode <- function(x, vocabulary = character(), unk_token = "[UNK]", max_input_chars_per_word = 100L){
+#' wordpiece_encode(x = c("unaffable", "unaffableun", "unknown territory"), 
+#'                  vocabulary = c("un", "##aff", "##able", "##un"),
+#'                  type = "ids") 
+wordpiece_encode <- function(x, vocabulary = character(), type = c("subwords", "ids"), unk_token = "[UNK]", max_input_chars_per_word = 100L){
+  type <- match.arg(type)
   max_input_chars_per_word <- as.integer(max_input_chars_per_word)
   unk_token <- as.character(unk_token)
   vocabulary <- as.character(vocabulary)
@@ -29,6 +35,11 @@ wordpiece_encode <- function(x, vocabulary = character(), unk_token = "[UNK]", m
     subwords <- unlist(subwords, use.names = FALSE)
     subwords
   })
+  if(type == "ids"){
+    x <- lapply(x, FUN = function(x){
+      match(x, vocabulary) - 1L
+    })
+  }
   x
 }
 
