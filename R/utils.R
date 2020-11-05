7e2184f4a681d136b176ff8d086622e74120c32a
Jan Wijffels <jwijffels@bnosac.be>
2020-11-05 10:13:37 +0100

    docs

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,15 @@
 
 
 #' @title Remove prefixed underscore
-#' @description Remove prefixed underscore unicode character \u2581
+#' @description Remove prefixed underscore unicode character 'LOWER ONE EIGHTH BLOCK' (U+2581)
 #' @param x a character vector
+#' @param replacement character string how to replace the underscore. Defaults to the empty string.
 #' @return \code{x} where the prefixed underscore is removed
 #' @export
 #' @examples
 #' x <- c("\u2581word", "hello", "_regularunderscore")
 #' x
 #' txt_remove_(x)
-txt_remove_ <- function(x){
-  gsub("^\u2581", "", x)
+txt_remove_ <- function(x, replacement = ""){
+  gsub(pattern = "^\u2581", replacement = replacement, x)
 }
